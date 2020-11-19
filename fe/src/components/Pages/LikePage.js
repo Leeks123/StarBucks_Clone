b39d900581e805d7b40b0a27e06f0b93ef0c536d
leeks123 <kensei719@naver.com>
2020-11-19 23:45:54 +0900

    Like Page Fin

--- fe/src/components/Pages/LikePage.js
+++ fe/src/components/Pages/LikePage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import styled, {keyframes} from "styled-components";
+import {withRouter} from "react-router-dom"
+import styled, {keyframes,css} from "styled-components";
 
 import find_wrap_bg from "../../images/like/findWrap_bg.jpg";
 
@@ -46,6 +47,24 @@ const tasteImg = [
   },
 ];
 
+const sectionMove = (sectionTop)=>keyframes`
+  from {
+    position :relative;
+    top : ${(sectionTop+1)*100}%;
+  }
+  to {
+    position :relative;
+    top : ${(sectionTop)*100}%;
+  }
+`
+const tastesMove = keyframes`
+from {
+  top : 350px;
+}
+to {
+  top : 150px;
+}
+`;
 const StyledContents = styled.div`
   padding-top: 120px;
   h1 {
@@ -64,14 +83,12 @@ const StyledContents = styled.div`
     .find_frame {
       max-width: 1100px;
       height : 100%;
+      overflow : hidden;
       margin: 0 auto;
-
+      position : relative;
       .find_section {
-        border : 1px solid red;
-
         width : 100%;
         height : 100%;
-        position :relative;
         & >img {
           position : relative;
           top : 85px;
@@ -80,7 +97,10 @@ const StyledContents = styled.div`
         }
         .find_taste {
           position : relative;
-          top : 150px;
+          
+          top : 350px;
+          animation : ${tastesMove} 1s linear forwards;
+
           display : flex;
           justify-content : space-around;
           margin : 30px;
@@ -95,6 +115,12 @@ const StyledContents = styled.div`
             }
           }
         }
+        ${(props) =>
+          props.sectionTop &&
+          css`
+            animation: ${sectionMove(props.sectionTop)} 1s linear forwards;
+          `}
+
       }
     }
   }
@@ -143,7 +169,9 @@ const StyledContents = styled.div`
       .find_frame {
         .find_section {
           & >img {
+            width : 76.5%;
             top : 50px;
+            margin-left : -38.25%;
           }
           .find_taste {
             top : 100px;
@@ -164,28 +192,55 @@ const StyledContents = styled.div`
   }
 `;
 
-const LikePage = () => {
+
+
+const LikePage = ({history}) => {
   const [flavor,setFlavor] = useState(0);
   const [feel,setFeel] = useState(0);
   const [strength,setStrength] = useState(0); 
+  const [sectionTop,setSectionTop] = useState(0);
 
   const onClickFn = {
-    0 : (e)=>{setFlavor(e.target.id)},
-    1 : (e)=>{setFeel(e.target.id)},
-    2 : (e)=>{setStrength(e.target.id)},
+    0 : (e)=>{
+      setFlavor(String(e.target.id));
+      setSectionTop(-1);
+    },
+    1 : (e)=>{
+      setFeel(String(e.target.id));
+      setSectionTop(-2);
+    },
+    2 : (e)=>{
+      setStrength(String(e.target.id))
+      console.log(flavor,feel,strength)
+      if(["000","010"].includes(flavor+feel+strength)) {
+       history.push("/coffee/beans/blond/크리스마스%20블론드%20로스트%20250g")
+      }else if(["002","020","012","022"].includes(flavor+feel+strength)) {
+        history.push("/coffee/beans/dark/크리스마스%20블렌드%20250g")
+      }else if(["100"].includes(flavor+feel+strength)) {
+        history.push("/coffee/beans/medium/르완다%20힝가카와%20250g")
+      }else if(["201"].includes(flavor+feel+strength)) {
+        history.push("/coffee/beans/medium/블랙퍼스트%20블렌드%20250g")
+      }else if(["011","211"].includes(flavor+feel+strength)) {
+        history.push("/coffee/beans/medium/하우스%20블렌드%20250g")
+      }else if(["202","122","212","222","302","312","322"].includes(flavor+feel+strength)) {
+        history.push("/coffee/beans/dark/에스프레소%20로스트%20250g")
+      }
+    },
   }
+
+
   return (
-    <StyledContents>
+    <StyledContents sectionTop={sectionTop}>
       <h1>나와 어울리는 커피</h1>
       <h4>여러분에게 잘 맞는 커피를 찾기 위한 3가지 질문입니다.</h4>
       <div className="find_wrap">
         <div className="find_frame">
           {tasteImg.map((section,i)=>(
-          <div className="find_section" key={i}>
+          <div className="find_section" key={i} >
             <img src={section.ttl} alt="ttl"/>
               <div className="find_taste"> 
                 {section.tastes.map((taste,j)=>(
-                  <div className="taste_item" id={j} onClick={onClickFn[i]}>
+                  <div className="taste_item" id={j} key={j} onClick={onClickFn[i]}>
                     <img src={taste.img} id={j} alt="taste"/>
                     <p id={j}>{taste.desc}</p>
                   </div>
@@ -201,4 +256,4 @@ const LikePage = () => {
   );
 };
 
-export default LikePage;
+export default withRouter(LikePage);
